034d92309f2b3ce0c81e33bd5187b1cd78843bab
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-09-16 04:32:00 +0700

    feat: add initialize response, result, server capabilities

--- lsp/initialize.go
+++ lsp/initialize.go
@@ -13,3 +13,42 @@ type ClientInfo struct {
 	Name    string `json:"name"`
 	Version string `json:"version"`
 }
+
+type InitializeResponse struct {
+	Response
+	Result InitializeResult `json:"result"`
+}
+
+type InitializeResult struct {
+	Capabilities ServerCapabilities `json:"capabilities"`
+	ServerInfo   ServerInfo         `json:"serverInfo"`
+}
+
+type ServerCapabilities struct {
+	TextDocumentSync int  `json:"textDocumentSync"`
+	HoverProvider    bool `json:"hoverProvider"`
+}
+
+type ServerInfo struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+func NewIninitializeResponse(id int) InitializeResponse {
+	return InitializeResponse{
+		Response: Response{
+			RPC: "2.0",
+			ID:  id,
+		},
+		Result: InitializeResult{
+			Capabilities: ServerCapabilities{
+				TextDocumentSync: 1,
+				HoverProvider:    true,
+			},
+			ServerInfo: ServerInfo{
+				Name:    "edulsp",
+				Version: "0.1",
+			},
+		},
+	}
+}
