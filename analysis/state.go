ad69b30e2636c179d382c5d135b94d79f8459e7d
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-09-16 04:29:08 +0700

    feat: add state

---
+++ analysis/state.go
@@ -0,0 +1,36 @@
+package analysis
+
+import (
+	"fmt"
+
+	"edulsp/lsp"
+)
+
+type State struct {
+	Documents map[string]string
+}
+
+func NewState() State {
+	return State{Documents: map[string]string{}}
+}
+
+func (s *State) OpenDocument(uri, text string) {
+	s.Documents[uri] = text
+}
+
+func (s *State) UpdateDocument(uri, text string) {
+	s.Documents[uri] = text
+}
+
+func (s *State) Hover(id int, uri string, position lsp.Position) lsp.HoverResponse {
+	document := s.Documents[uri]
+	return lsp.HoverResponse{
+		Response: lsp.Response{
+			RPC: "2.0",
+			ID:  id,
+		},
+		Result: lsp.HoverResult{
+			Contents: fmt.Sprintf("File: %s, Characters: %d", uri, len(document)),
+		},
+	}
+}
