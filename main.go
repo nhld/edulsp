7c2dc60793ab2efa821be04c301d7c6b2b423727
Nhan Luu <62146587+nhld@users.noreply.github.com>
2024-09-16 04:33:48 +0700

    feat: add textdocument

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
+	"edulsp/analysis"
 	"edulsp/lsp"
 	"edulsp/rpc"
 )
@@ -18,6 +20,9 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
 
+	state := analysis.NewState()
+	writer := os.Stdout
+
 	for scanner.Scan() {
 		msg := scanner.Bytes()
 		method, contents, err := rpc.DecodingMessage(msg)
@@ -25,11 +30,11 @@ func main() {
 			logger.Printf("Error: %s", err)
 			continue
 		}
-		handleMessage(logger, method, contents)
+		handleMessage(logger, writer, state, method, contents)
 	}
 }
 
-func handleMessage(logger *log.Logger, method string, contents []byte) {
+func handleMessage(logger *log.Logger, writer io.Writer, state analysis.State, method string, contents []byte) {
 	logger.Printf("Received method: %s", method)
 	switch method {
 	case "initialize":
@@ -38,7 +43,55 @@ func handleMessage(logger *log.Logger, method string, contents []byte) {
 			logger.Printf("Cant parse: %s", err)
 		}
 		logger.Printf("Connected to: %s %s", request.Params.ClientInfo.Name, request.Params.ClientInfo.Version)
+
+		msg := lsp.NewIninitializeResponse(request.ID)
+		err := writeResponse(writer, msg)
+		if err != nil {
+			log.Printf("Failed to write response: %v", err)
+		}
+		logger.Print("Sent reply")
+
+	case "textDocument/didOpen":
+		var request lsp.DidOpenTextDocumentNotification
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Printf("textDocument/didOpen: %s", err)
+			return
+		}
+		logger.Printf("Opened: %s", request.Params.TextDocument.URI)
+		state.OpenDocument(request.Params.TextDocument.URI, request.Params.TextDocument.Text)
+
+	case "textDocument/didChange":
+		var request lsp.TextDocumentDidChangeNotification
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Printf("textDocument/didChange: %s", err)
+			return
+		}
+		logger.Printf("Changed: %s", request.Params.TextDocument.URI)
+		for _, change := range request.Params.ContentChanges {
+			state.UpdateDocument(request.Params.TextDocument.URI, change.Text)
+		}
+
+	case "textDocument/hover":
+		var request lsp.HoverRequest
+		if err := json.Unmarshal(contents, &request); err != nil {
+			logger.Printf("textDocument/hover: %s", err)
+			return
+		}
+		reponse := state.Hover(request.ID, request.Params.TextDocument.URI, request.Params.Position)
+		err := writeResponse(writer, reponse)
+		if err != nil {
+			log.Printf("Failed to write response: %v", err)
+		}
 	}
+}
+
+func writeResponse(writer io.Writer, msg any) error {
+	reply := rpc.EncodingMessage(msg)
+	_, err := writer.Write([]byte(reply))
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func getLogger(filename string) *log.Logger {
